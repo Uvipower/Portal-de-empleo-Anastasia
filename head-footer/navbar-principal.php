1a806dfe1cbb3a595a8d4cf344e110969cbe8855
Uvipower <joel.nahim.rivera.perez@gmail.com>
2015-10-27 19:36:24 -0300

    Version con codigos postales

---
+++ head-footer/navbar-principal.php
@@ -0,0 +1,49 @@
+<!--Inicio del Navbar-->
+<nav class="navbar navbar-default" role="navigation">
+	<div class="container">
+		<!--Logo y boton movil que agrupa el contenido-->
+		<div class="navbar-header">
+			<button type="botton" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse1">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="pull-left" href="index.php"><img src="images/logo.png" alt="Brand" class="img-responsive" id="logo"></a>
+		</div>
+		<!-- Links, forms, y otros elementos para el toggle-->
+		<div class="collapse navbar-collapse" id="navbar-co	llapse1">
+			<ul class="nav navbar-nav">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Productos en celulares <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Nokia</a></li>
+						<li><a href="#">Motorola</a></li>
+						<li class="divider"></li>
+						<li><a href="#">Etc...</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Noticias</a></li>
+				<li><a href="#">Operadora</a></li>
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">Comunidad <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Versus!</a></li>
+						<li><a href="#">Compara...</a></li>
+					</ul>
+				</li>
+			</ul>
+			<form class="navbar-form navbar-left" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Buscar...">
+				</div>
+			</form>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#"><i class="fa fa-facebook-square fa-2x"></i></a></li>
+				<li><a href="#"><i class="fa fa-google-plus fa-2x"></i></a></li>
+				<li><a href="#"><i class="fa fa-rss fa-2x"></i></a></li>
+				<li><a href="#"><i class="fa fa-envelope-o fa-2x"></i></a></li>
+			</ul>
+		</div><!-- .navbar-collapse-->
+	</div><!-- .container-fluid-->	
+</nav><!--Fin del Navbar-->
